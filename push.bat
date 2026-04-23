14e990c3a62bd198324ced3569aed90b559415a0
WB Canteen Team <team@wbcanteen.com>
2026-04-23 14:55:51 +0700

    Add railway.toml configuration

---
+++ push.bat
@@ -0,0 +1,14 @@
+@echo off
+
+REM 执行 Git 命令
+echo 正在添加文件...
+"D:\Program Files\Git\bin\git.exe" add .
+
+echo 正在提交更改...
+"D:\Program Files\Git\bin\git.exe" commit -m "Add railway.toml configuration"
+
+echo 正在推送到 GitHub...
+"D:\Program Files\Git\bin\git.exe" push origin main
+
+echo 操作完成！
+pause
